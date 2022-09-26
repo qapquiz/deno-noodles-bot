209de43409aef08d4f036f2797a438af91b334fb
armariya <ariya.l@dv.co.th>
2022-09-26 13:27:52 +0700

    feat: minimal bot

--- main.ts
+++ main.ts
@@ -1,8 +1,23 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
+import {
+  Bot,
+  Intents,
+  startBot,
+  createBot,
+  Message,
+} from "https://deno.land/x/discordeno@16.0.1/mod.ts";
 
-// Learn more at https://deno.land/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
-}
+const bot = createBot({
+  token: Deno.env.get("DISCORD_TOKEN") ?? "",
+  intents: Intents.Guilds | Intents.GuildMessages,
+  events: {
+    ready() {
+      console.log("Successfully connected to gateway");
+    },
+  },
+});
+
+bot.events.messageCreate = function (b: Bot, message: Message) {
+  // Process the message here with your command handler.
+};
+
+await startBot(bot);
